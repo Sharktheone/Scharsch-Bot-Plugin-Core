f44f05c2f7d8c198d599cbb1f1631a939cbd5c7f
Sharktheone <sharktheone@proton.me>
2023-04-24 00:09:44 +0200

    basic event handlers

---
+++ src/config/mc_events.rs
@@ -0,0 +1,14 @@
+
+
+
+fn player_join(name: String) {
+    println!("Player joined: {}", name);
+}
+
+fn player_leave(name: String) {
+    println!("Player left: {}", name);
+}
+
+fn player_chat(name: String, message: String) {
+    println!("Player chat: {} - {}", name, message);
+}
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,2 +1,4 @@
 pub mod config_format;
 pub mod load;
+mod ws_events;
+mod mc_events;
