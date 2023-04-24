82ca8a4d6512c3744efdb0f77044da3592dede60
Sharktheone <sharktheone@proton.me>
2023-04-24 22:26:11 +0200

    make const public

--- src/events/message.rs
+++ src/events/message.rs
@@ -1,23 +1,23 @@
 use serde::{Deserialize, Serialize};
 
 
-const SEND_PLAYERS: &str = "sendPlayers";
-const KICK_PLAYER: &str = "kickPlayer";
-const REPORT_PLAYER: &str = "reportPlayer";
-const BAN_PLAYER: &str = "banPlayer";
-const UNBAN_PLAYER: &str = "unbanPlayer";
-const SEND_COMMAND: &str = "sendCommand";
-const SEND_CHAT_MESSAGE: &str = "sendChatMessage";
-const PLAYER_JOINED: &str = "playerJoined";
-const PLAYER_LEFT: &str = "playerLeft";
-const PLAYERS: &str = "players";
-const CHAT_MESSAGE: &str = "chatMessage";
-const PLAYER_DEATH: &str = "playerDeath";
-const PLAYER_ADVANCEMENT: &str = "playerAdvancement";
-const AUTH: &str = "auth";
-const AUTH_SUCCESS: &str = "authSuccess";
-const AUTH_FAILED: &str = "authFailed";
-const ERROR: &str = "error";
+pub const SEND_PLAYERS: &str = "sendPlayers";
+pub const KICK_PLAYER: &str = "kickPlayer";
+pub const REPORT_PLAYER: &str = "reportPlayer";
+pub const BAN_PLAYER: &str = "banPlayer";
+pub const UNBAN_PLAYER: &str = "unbanPlayer";
+pub const SEND_COMMAND: &str = "sendCommand";
+pub const SEND_CHAT_MESSAGE: &str = "sendChatMessage";
+pub const PLAYER_JOINED: &str = "playerJoined";
+pub const PLAYER_LEFT: &str = "playerLeft";
+pub const PLAYERS: &str = "players";
+pub const CHAT_MESSAGE: &str = "chatMessage";
+pub const PLAYER_DEATH: &str = "playerDeath";
+pub const PLAYER_ADVANCEMENT: &str = "playerAdvancement";
+pub const AUTH: &str = "auth";
+pub const AUTH_SUCCESS: &str = "authSuccess";
+pub const AUTH_FAILED: &str = "authFailed";
+pub const ERROR: &str = "error";
 
 
 #[derive(Debug, Deserialize, Serialize)]
