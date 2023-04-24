6d70703d1a4cc26cf85fa0a5e2c40a418e19c842
Sharktheone <sharktheone@proton.me>
2023-04-24 15:09:46 +0200

    add Event consts

--- src/events/message.rs
+++ src/events/message.rs
@@ -1,6 +1,24 @@
 use serde::{Deserialize, Serialize};
 
 
+const SEND_PLAYERS: &str = "sendPlayers";
+const KICK_PLAYER: &str = "kickPlayer";
+const REPORT_PLAYER: &str = "reportPlayer";
+const BAN_PLAYER: &str = "banPlayer";
+const UNBAN_PLAYER: &str = "unbanPlayer";
+const SEND_COMMAND: &str = "sendCommand";
+const SEND_CHAT_MESSAGE: &str = "sendChatMessage";
+const PLAYER_JOINED: &str = "playerJoined";
+const PLAYER_LEFT: &str = "playerLeft";
+const PLAYERS: &str = "players";
+const CHAT_MESSAGE: &str = "chatMessage";
+const PLAYER_DEATH: &str = "playerDeath";
+const PLAYER_ADVANCEMENT: &str = "playerAdvancement";
+const AUTH: &str = "auth";
+const AUTH_SUCCESS: &str = "authSuccess";
+const AUTH_FAILED: &str = "authFailed";
+const ERROR: &str = "error";
+
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Message {
