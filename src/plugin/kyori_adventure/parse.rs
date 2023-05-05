34d1a22feebd4e3c02f5f0b500b3f3a4e3f7d320
Sharktheone <sharktheone@proton.me>
2023-05-06 00:06:05 +0200

    parse full component

--- src/plugin/kyori_adventure/parse.rs
+++ src/plugin/kyori_adventure/parse.rs
@@ -1,11 +1,55 @@
-use jni::objects::JObject;
+use jni::objects::{JObject, JValue};
+use crate::jni_utils::{call_stacking, get_env, JniFn, JSTRING};
+use crate::plugin::logger::error;
 
-pub fn parse<'a, S: Into<String>>(msg: S) -> JObject<'a> {
-    let _msg = msg.into();
+pub fn parse<'a, S: Into<String>>(component: S) -> Result<JObject<'a>, ()> {
+    let component = component.into();
 
+    let mut env = match get_env() {
+        Ok(env) => env,
+        Err(_) => {
+            error("Error getting env");
+            return Err(());
+        },
+    };
 
-    // use gson parser to deserialize the message
+    let serializer = match env.find_class("net/kyori/adventure.text/serializer/gson/GsonComponentSerializer"){
+        Ok(serializer) => serializer,
+        Err(err) => {
+            error(format!("Error creating serializer: {}", err));
+            return Err(());
+        },
+    };
 
+    let msg_string = match env.new_string(component) {
+        Ok(msg_string) => msg_string,
+        Err(err) => {
+            error(format!("Error creating msg string: {}", err));
+            return Err(());
+        },
+    };
 
-    JObject::null()
+    let msg_obj = JObject::from(msg_string);
+    let arg: JValue = JValue::Object(&msg_obj);
+
+    let fns = [
+        JniFn {
+            name: "gson",
+            input: &[],
+            output: "net.kyori.adventure.text.serializer.gson.GsonComponentSerializer",
+            args: &[],
+        },
+        JniFn {
+            name: "deserialize",
+            input: &[JSTRING],
+            output: "net.kyori.adventure.text.Component",
+            args: &[arg],
+        },
+    ];
+
+    let final_component = call_stacking(&serializer, &fns);
+
+    unsafe {
+        Ok(JObject::from_raw(final_component.as_raw()))
+    }
 }
